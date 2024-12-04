c1f3fc037a619b99c952b2c22b8e9af889a374dc
polkolsollol <kuba.bochenski@vp.pl>
2024-12-04 22:28:32 +0100

    Changess

--- src/my_project_backend/src/lib.rs
+++ src/my_project_backend/src/lib.rs
@@ -10,9 +10,16 @@ thread_local! {
     static CONFIG: RefCell<Config> = RefCell::new(Config::new());
     static BLOGS: RefCell<Vec<Blog>> = RefCell::new(Vec::new());
 }
+// komentarze ?
+
+#[ic_cdk::update]
+fn add_config(new_config: Config) {
+    CONFIG.with(|config| *config.borrow_mut() = new_config);
+}
 
 #[ic_cdk::update]
 fn add_blog(title: String, content: String, tags: Vec<String>) -> Result<Blog, String>{
+    ic_cdk::println!("Trying to add blog: (title: {}, content: {}, tags: {:?})", title, content, tags);
     let config = CONFIG.with(|config| config.borrow().clone());
     if title.len() > config.max_title_len as usize {
         return Err("Title is too long!".to_string())
@@ -23,6 +30,11 @@ fn add_blog(title: String, content: String, tags: Vec<String>) -> Result<Blog, S
     if tags.len() > config.max_tags_count as usize {
         return Err("Too many tags!".to_string())
     }
+    let are_tags_in_config_tags = tags.iter().any(|tag| !config.tags.contains(tag));
+    if are_tags_in_config_tags {
+        return Err("Tags are not valid!".to_string()) 
+    }
+    
     let blog = Blog::new(title, content, tags);
     BLOGS.with(|blogs| blogs.borrow_mut().push(blog));
     let last_blog = BLOGS.with(|blogs| 
